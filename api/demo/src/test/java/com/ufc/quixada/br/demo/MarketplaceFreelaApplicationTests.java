810636bb044473289c2b65d8621327fa9848a4c8
gustavohenriquefs <gustavohenriquefs.dev@gmail.com>
2025-11-23 12:52:33 -0300

    feat: setup api

---
+++ api/demo/src/test/java/com/ufc/quixada/br/demo/MarketplaceFreelaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ufc.quixada.br.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarketplaceFreelaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
